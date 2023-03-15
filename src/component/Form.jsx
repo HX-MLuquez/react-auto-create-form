c2f8c03a675500f2d79d8107b1f0d1e69739015a
Mauricio776 <mauuuricio@hotmail.com>
2023-03-15 18:40:26 -0300

    finish fixed

--- src/component/Form.jsx
+++ src/component/Form.jsx
@@ -1,9 +1,24 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./Form.css";
-import Parts from "./Parts";
+import Part from "./Part";
 
 const Form = () => {
-  // Define initial state for form inputs
+  const [data, setData] = useState();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch("../../utils/db.json");
+      console.log(response);
+      const jsonData = await response.json();
+      console.log(jsonData.items);
+      setData(jsonData.items);
+    };
+    fetchData();
+  }, []);
+
+  var type = "input";
+  let id = 1;
+
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [message, setMessage] = useState("");
@@ -14,11 +29,33 @@ const Form = () => {
     console.log(`Name: ${name}\nEmail: ${email}\nMessage: ${message}`);
     // Perform additional form submission logic here
   };
+  
+  const handleChangeInput = function(){
 
+  }
   return (
-    <form className="form-component" onSubmit={handleSubmit}>
-      <Parts></Parts>
-    </form>
+    <div>
+      <form className="form-component" onSubmit={handleSubmit}>
+        {type !== "submit" ? (
+          <div className="form-body">
+            <div className="form-group">
+              <div className="form-header">
+                <h2>Title</h2>
+              </div>
+            </div>
+            {data?.map((e) => (
+              <Part key={id++ + e.name} props={e} handleChangeInput={handleChangeInput}></Part>
+            ))}
+          </div>
+        ) : (
+          <div className="form-footer">
+            <button type="submit" >
+              Submit
+            </button>
+          </div>
+        )}
+      </form>
+    </div>
   );
 };
 
